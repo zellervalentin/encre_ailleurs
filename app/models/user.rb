73dd311d458edeec3f23981c903fd035dbfcd959
Valentin Zeller <foxyjr@live.fr>
2017-03-29 16:04:54 +0200

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@
 #
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :letters
+
   def to_s
     "#{first_name} #{last_name}"
   end
