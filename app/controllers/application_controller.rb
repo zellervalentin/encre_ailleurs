675d05b7e32368ceabc56daf55b32da5752bc8ff
Valentin Zeller <foxyjr@live.fr>
2017-03-30 15:32:54 +0200

    fixed self letter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, :if => :devise_controller?
   before_action :authenticate_user!
-
-    before_filter :configure_permitted_parameters, :only => [:create]
 
   protected
 
