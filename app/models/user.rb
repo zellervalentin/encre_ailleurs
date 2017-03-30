ea7fe9262719a51d2727532472ec3b281a5d0aa5
Valentin Zeller <foxyjr@live.fr>
2017-03-30 14:29:10 +0200

    removed avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,6 @@ class User < ApplicationRecord
   validates_presence_of :first_name
   validates_presence_of :last_name
   has_many :letters
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def to_s
     "#{first_name} #{last_name}"
