5b6308520cb0d6719e08c94808afb41c885045e5
Valentin Zeller <foxyjr@live.fr>
2017-03-31 13:54:37 +0200

    Blue

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :users
   get "/pages/:page" => "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'letters#index'
 end
